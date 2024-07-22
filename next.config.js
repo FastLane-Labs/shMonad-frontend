ec15375539484eeb158d2d4be26e3dd1b7981d30
jj1980a <j.feral@live.fr>
2024-07-22 16:42:46 +0400

    enable static exports

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+  webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    if (config.cache && !dev) {
-      config.cache = Object.freeze({
-        type: 'memory',
-      })
-      config.cache.maxMemoryGenerations = 0
-    }
     return config
   },
   images: {
     remotePatterns: [{ hostname: '*' }],
   },
+  output: 'export',
 }
 
 module.exports = nextConfig
