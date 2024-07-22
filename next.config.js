b63e9a66a01637d1ea144b569b74d03ee142ecee
jj1980a <j.feral@live.fr>
2024-07-22 16:26:11 +0400

    cache build config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      })
+      config.cache.maxMemoryGenerations = 0
+    }
     return config
   },
   images: {
     remotePatterns: [{ hostname: '*' }],
   },
-  output: 'standalone',
 }
 
 module.exports = nextConfig
