6e58b2d8ba59f4970173b3116266c8463409082c
0xNotMe <nme15500@gmail.com>
2024-07-25 13:43:04 +1000

    fix: unit tests

--- __tests__/page.test.jsx
+++ __tests__/page.test.jsx
@@ -1,17 +1,9 @@
-import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import Page from '../src/app/page'
- 
-describe('Page', () => {
-    it('empty test', () => {
-        expect(1).toBe(1)
-    })
 
-//   it('renders a heading', () => {
-//     render(<Page />)
- 
-//     const heading = screen.getByRole('heading', { level: 1 })
- 
-//     expect(heading).toBeInTheDocument()
-//   })
+describe('Page', () => {
+  it('empty test', () => {
+    expect(1).toBe(1)
+  })
 })
