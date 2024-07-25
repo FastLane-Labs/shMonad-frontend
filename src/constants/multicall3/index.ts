02fb23dff59a4e2599675d121564f3d1d9b720c3
jj1980a <j.feral@live.fr>
2024-07-25 12:30:13 +0400

    multicall3 constant

---
+++ src/constants/multicall3/index.ts
@@ -0,0 +1,6 @@
+import MULTICALL3_ABI from './abi.json'
+
+// Same address for over 100 networks
+const MULTICALL3_ADDR = '0xcA11bde05977b3631167028862bE2a173976CA11'
+
+export { MULTICALL3_ABI, MULTICALL3_ADDR }
