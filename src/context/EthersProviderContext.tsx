458e65bf28dd9524d9cc0512ca98f6a289b8afcf
0xNotMe <nme15500@gmail.com>
2024-07-27 20:08:55 +1000

    refactor: useProviderContext return type

--- src/context/EthersProviderContext.tsx
+++ src/context/EthersProviderContext.tsx
@@ -4,23 +4,17 @@ import { JsonRpcProvider, FallbackProvider } from 'ethers'
 import { useChainId } from 'wagmi'
 import { useEthersProvider } from '@/hooks/useEthersProvider'
 
-export type EthersProviderType = JsonRpcProvider | FallbackProvider | undefined
-
-interface EthersProviderContextType {
-  ethersProvider: EthersProviderType
-}
-
-const EthersProviderContext = createContext<EthersProviderContextType | null>(null)
+const EthersProviderContext = createContext<JsonRpcProvider | FallbackProvider | undefined>(undefined)
 
 export const EthersProviderWrapper: React.FC<{ children: ReactNode }> = ({ children }) => {
   const chainId = useChainId()
   const ethersProvider = useEthersProvider({ chainId })
-  const value = useMemo(() => ({ ethersProvider }), [ethersProvider])
+  const value = useMemo(() => ethersProvider, [ethersProvider])
 
   return <EthersProviderContext.Provider value={value}>{children}</EthersProviderContext.Provider>
 }
 
-export const useEthersProviderContext = () => {
+export const useEthersProviderContext = (): JsonRpcProvider | FallbackProvider => {
   const context = useContext(EthersProviderContext)
   if (!context) {
     throw new Error('useEthersProviderContext must be used within an EthersProviderWrapper')
