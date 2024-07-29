cc6ba8d5ed2303fc56ecd559ee83670b4aac8f76
0xNotMe <nme15500@gmail.com>
2024-07-29 17:37:43 +1000

    fix: initial loading issue

--- src/components/Buttons/SwapButton.tsx
+++ src/components/Buttons/SwapButton.tsx
@@ -1,21 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useConnectModal } from '@rainbow-me/rainbowkit'
 import { useAccount } from 'wagmi'
 import { useBalance } from '@/hooks/useBalance'
 import { useSwapContext } from '@/context/SwapContext'
 
 interface SwapButtonProps {
-  isConnected: boolean
   handleSwap: () => Promise<void>
   isLoading: boolean
 }
 
-const SwapButton: React.FC<SwapButtonProps> = ({ isConnected, handleSwap, isLoading }) => {
+const SwapButton: React.FC<SwapButtonProps> = ({ handleSwap, isLoading }) => {
   const { openConnectModal } = useConnectModal()
   const { fromToken, toToken, fromAmount, quoteLoading } = useSwapContext()
-  const { address: userAddress } = useAccount()
+  const { address: userAddress, status, isConnecting, isConnected } = useAccount()
   const [localLoading, setLocalLoading] = useState(false)
+  const [initialized, setInitialized] = useState(false)
   const { data: balance, isLoading: balanceLoading } = useBalance({ token: fromToken!, userAddress: userAddress! })
+
+  useEffect(() => {
+    if (status !== 'connecting') {
+      setInitialized(true)
+    }
+  }, [status])
 
   const handleClick = async () => {
     setLocalLoading(true)
@@ -23,10 +29,24 @@ const SwapButton: React.FC<SwapButtonProps> = ({ isConnected, handleSwap, isLoad
     setLocalLoading(false)
   }
 
-  if (!isConnected) {
+  if (!initialized) {
+    return (
+      <button className='btn rounded-2xl w-full' disabled>
+        Reconnecting to Wallet
+      </button>
+    )
+  }
+
+  if (!isConnected && status != 'reconnecting') {
     return (
       <button className='btn rounded-2xl w-full' onClick={() => openConnectModal?.()}>
-        {isLoading ? 'Connecting to Wallet' : 'Connect wallet'}
+        Connect wallet
+      </button>
+    )
+  } else if (status === 'reconnecting') {
+    return (
+      <button className='btn rounded-2xl w-full' disabled>
+        Reconnecting to Wallet
       </button>
     )
   } else if (!fromToken || !toToken) {
