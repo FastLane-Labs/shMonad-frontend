610470307d8ec9d5347a3b5cd8a97d7dcd8e239e
jj1980a <j.feral@live.fr>
2024-08-01 14:56:43 +0400

    fix maxfeepergas calculation

--- src/hooks/useHandleSwap.ts
+++ src/hooks/useHandleSwap.ts
@@ -12,18 +12,19 @@ import {
 import { useFastLaneOnline } from './useFastLaneOnline'
 import { ethers } from 'ethers'
 import { FastlaneOnlineAbi } from '@/abis'
-import { getFeeData } from '@/utils/gasFee'
 import { Address } from 'viem'
+import { getFeeData } from '@/utils/gasFee'
+import { getAtlasGasSurcharge } from '@/utils/atlas'
 
 export const useHandleSwap = () => {
   const { signer, provider } = useEthersProviderContext()
   const { address } = useAccount()
   const { quote, quoteLoading } = useSwapContext()
   const [isSwapping, setIsSwapping] = useState(false)
-  const { dappAddress, verificationAddress } = useFastLaneOnline()
+  const { atlasAddress, dappAddress, atlasVerificationAddress } = useFastLaneOnline()
 
   const handleSwap = useCallback(async () => {
-    if (!address || !provider || !quote || quoteLoading || !dappAddress || !verificationAddress) {
+    if (!address || !provider || !quote || quoteLoading || !atlasAddress || !dappAddress || !atlasVerificationAddress) {
       console.error('Missing required data for swap')
       return false
     }
@@ -33,27 +34,26 @@ export const useHandleSwap = () => {
       // Build swap intent
       const swapIntent = buildSwapIntent(quote)
 
-      // Hardcoded - no good
       const executionEnvironment = await getExecutionEnvironment(
-        '0x892F8f6779ca6927c1A6Cc74319e03d2abEf18D5',
+        atlasAddress as Address,
         dappAddress as Address,
-        dappAddress as Address,
+        dappAddress as Address, // user is also the dapp address
         provider
       )
 
       // Build baseline call data
       const baselineCall = await buildBaselineCallData(quote, executionEnvironment)
 
-      // Prepare other parameters for user operation
+      const block = await provider.getBlock('latest')
       const feeData = await getFeeData(provider)
       if (!feeData.maxFeePerGas || !feeData.gasPrice) {
         console.error('Missing required data for swap')
         return false
       }
 
-      const maxFeePerGas = feeData.maxFeePerGas
-      const deadline = Date.now() + 3600000 // Example: set deadline to 1 hour from now
-      const gas = 1500000n
+      const maxFeePerGas = feeData.maxFeePerGas * 2n
+      const deadline = block?.number! + 200 // Example: set deadline 200 blocks away
+      const gas = 2000000n // Example gas limit, adjust as needed
 
       // Build user operation
       const userOperation = await buildUserOperation(
@@ -67,11 +67,13 @@ export const useHandleSwap = () => {
         provider
       )
       //Get user operation hash
-      const userOpHash = await getUserOperationHash(userOperation, verificationAddress, provider)
+      const userOpHash = await getUserOperationHash(userOperation, atlasVerificationAddress, provider)
 
       const contract = new ethers.Contract(dappAddress, FastlaneOnlineAbi, signer)
       const tx = await contract.fastOnlineSwap(swapIntent, baselineCall, deadline, gas, maxFeePerGas, userOpHash, {
         gasLimit: gas,
+        maxFeePerGas: maxFeePerGas,
+        value: getAtlasGasSurcharge(gas * maxFeePerGas),
       })
 
       console.log('Swap transaction submitted:', tx.hash)
@@ -85,7 +87,7 @@ export const useHandleSwap = () => {
     } finally {
       setIsSwapping(false)
     }
-  }, [address, provider, quote, quoteLoading, dappAddress, verificationAddress])
+  }, [address, provider, quote, quoteLoading, dappAddress, atlasVerificationAddress])
 
   return {
     handleSwap,
