6b4c0e39f373dd8a6f387619f386013aef8d437e
Eddie Salcedo <inscentia@yahoo.com>
2024-08-04 19:57:49 -0700

    store iux modal open/close in localstorage

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { NetworkStatus } from './NetworkStatus'
+import { QuestionMarkCircleIcon } from '@heroicons/react/24/outline'
 
 export function Footer() {
   return (
     <>
       <div className='place-self-end'>
-        <NetworkStatus />
+        <div className='flex items-center gap-1 p-4'>
+          <NetworkStatus />
+          {/* <button className=''>
+            <QuestionMarkCircleIcon className='h-5 w-5 mb-px text-gray-400' />
+          </button> */}
+        </div>
       </div>
     </>
   )
--- src/views/SwapView.tsx
+++ src/views/SwapView.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import FlipButton from '@/components/Buttons/FlipButton'
 import SettingsButton from '@/components/Buttons/SettingsButton'
 import SwapButton from '@/components/Buttons/SwapButton'
@@ -12,41 +12,56 @@ import IUXModal from '@/components/iUX/IUX'
 
 const SwapView: React.FC = () => {
   const [isSettingsModalVisible, setIsSettingsModalVisible] = useState<boolean>(false)
-  const [isIUXModalVisible, setIsIUXModalVisible] = useState<boolean>(true)
+  const [isIUXModalVisible, setIsIUXModalVisible] = useState<boolean>(false)
+  const [isLoaded, setIsLoaded] = useState<boolean>(false)
+
+  useEffect(() => {
+    const isModalClosed = localStorage.getItem('isIUXModalClosed')
+    if (!isModalClosed) {
+      setIsIUXModalVisible(true)
+    }
+    setIsLoaded(true) // Indicate that the loading is complete
+  }, [])
+
+  const handleIUXModalClose = () => {
+    localStorage.setItem('isIUXModalClosed', 'true')
+    setIsIUXModalVisible(false)
+  }
+
+  const handleIUXModalOpen = () => {
+    localStorage.removeItem('isIUXModalClosed')
+    setIsIUXModalVisible(true)
+  }
 
   const { handleSwap, isSwapping } = useHandleSwap()
   const isQuoteLoading = useBaselineQuote()
 
+  if (!isLoaded) {
+    // Render nothing or a loading indicator while loading
+    return null
+  }
+
   return (
     <div className='relative max-w-md mx-auto'>
-      <IUXModal isVisible={isIUXModalVisible} onClose={() => setIsIUXModalVisible(false)} />
+      <IUXModal isVisible={isIUXModalVisible} onClose={handleIUXModalClose} />
       <BackgroundGradient />
       <div
         className='relative rounded-3xl bg-base-200/30'
-        style={
-          {
-            // background: 'rgba(0, 0, 0, .3)',
-          }
-        }>
-        <div
-          className='gradient-bg relative max-w-md mx-auto p-4 rounded-3xl border border-accent'
-          style={{
-            // background: 'linear-gradient(290deg, rgba(241,35,121, .1) 10%, rgba(7, 76, 255 , .1) 100%)',
-            boxShadow: 'rgba(241, 32, 116, .2) 0px 5px 90px 4px',
-          }}>
+        style={{ boxShadow: 'rgba(241, 32, 116, .2) 0px 5px 90px 4px' }}>
+        <div className='gradient-bg relative max-w-md mx-auto p-4 rounded-3xl border border-accent'>
           <div className='flex justify-end items-center mb-2'>
             <SettingsButton setIsSettingsModalVisible={setIsSettingsModalVisible} />
           </div>
-
           <SellComponent />
           <FlipButton />
           <BuyComponent />
-
           <SwapButton handleSwap={handleSwap} isLoading={isSwapping || isQuoteLoading} />
-
           <SettingsModal isVisible={isSettingsModalVisible} onClose={() => setIsSettingsModalVisible(false)} />
         </div>
       </div>
+      {/* <button className='fixed bottom-4 right-4 bg-blue-500 text-white p-2 rounded' onClick={handleIUXModalOpen}>
+        Open Modal
+      </button> */}
     </div>
   )
 }
