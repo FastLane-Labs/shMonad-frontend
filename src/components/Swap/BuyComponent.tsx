a4c1e4c0714c31bbeca8e5cc2a42a582301bd37b
Eddie Salcedo <inscentia@yahoo.com>
2024-08-06 19:35:58 -0700

    set sell token balance

--- src/components/Swap/BuyComponent.tsx
+++ src/components/Swap/BuyComponent.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import BuyAmount from './BuyAmount'
 import { useSwapStateContext } from '@/context/SwapStateContext'
+import { TokenBalance } from '@/components/TokenBalance/TokenBalance'
 
 const BuyComponent: React.FC = () => {
-  const { toToken, setToToken, toAmount, setToAmount, isQuoteing, setSwapDirection } = useSwapStateContext()
+  const {
+    toToken: buyToken,
+    setToToken: setBuyToken,
+    toAmount: buyAmount,
+    setToAmount: setBuyAmount,
+    isQuoteing,
+    setSwapDirection,
+  } = useSwapStateContext()
 
   return (
     <div className='input-card mb-4'>
       <div className='flex justify-between items-center mb-2 text-sm'>
         <span className='text-base-content'>To</span>
+        <h1 className='text-base-content'>
+          <span>Balance: </span>
+          <TokenBalance token={buyToken || undefined} toFixed={4} />
+        </h1>
       </div>
       <BuyAmount
-        buyToken={toToken}
-        setBuyToken={setToToken}
-        buyAmount={toAmount}
-        setBuyAmount={setToAmount}
+        buyToken={buyToken}
+        setBuyToken={setBuyToken}
+        buyAmount={buyAmount}
+        setBuyAmount={setBuyAmount}
         quoteLoading={isQuoteing}
         setSwapDirection={setSwapDirection}
       />
