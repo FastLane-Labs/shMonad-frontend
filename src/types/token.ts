e85a02baa74ba0e07fd6736ff3a1b60b5b8fdfa6
Eddie Salcedo <inscentia@yahoo.com>
2024-08-07 12:46:26 -0700

    add formattedBalance to type TokenWithBalance

--- src/types/token.ts
+++ src/types/token.ts
@@ -11,7 +11,10 @@ export type Token = {
   tags?: string[]
 }
 
-export type TokenWithBalance = Token & { balance: string }
+export type TokenWithBalance = Token & {
+  balance: string
+  formattedBalance?: string
+}
 
 export interface ITokenProvider {
   getTokensByChainId(chainId: number): Promise<Token[]>
