5d967b8ca27030f0c3db1425dc60612960e96368
Eddie Salcedo <inscentia@yahoo.com>
2024-08-07 22:33:44 -0700

    minor iux ui fix

--- src/components/Modals/IUX.tsx
+++ src/components/Modals/IUX.tsx
@@ -11,7 +11,7 @@ const IUXModal: React.FC<IUXModalProps> = ({ isVisible, onClose }) => {
   }
   return (
     <ModalWrapper isVisible={isVisible} onClose={closeModal} style={{ paddingBottom: '28px' }}>
-      <div className='flex overflow-scroll text-neutral-content flex-col grid-cols-1 justify-start pt-4 gap-4 xl:pl-0 xl:pr-0'>
+      <div className='flex overflow-scroll md:overflow-visible -mr-2.5 md:mr-0 text-neutral-content flex-col grid-cols-1 justify-start pt-4 gap-4 xl:pl-0 xl:pr-0'>
         <h2 className='text-5xl'>MEV Powered Swap Quotes</h2>
         <div className='gray-text text-xl'>
           Boost your swap quotes lorem ipsum dolor sit amet, consectetur adipiscing elit
