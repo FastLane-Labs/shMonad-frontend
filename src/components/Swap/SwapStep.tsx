56fe1a6b13a2674273282d3920c840abb5f2dd5d
Eddie Salcedo <inscentia@yahoo.com>
2024-08-08 01:30:26 -0700

    swap modal: add spinner to button when isLoading

--- src/components/Swap/SwapStep.tsx
+++ src/components/Swap/SwapStep.tsx
@@ -166,17 +166,43 @@ const SwapStep: React.FC<SwapStepProps> = ({ step, onAction, isLoading, error, s
   )
 
   const renderButton = () => {
-    let buttonText = ''
+    let buttonText: string | JSX.Element = ''
     let action: 'approve' | 'sign' | 'swap' = 'approve'
     let isDisabled = isLoading || isSwapping
 
     if (isSwapping) {
       buttonText = 'Proceed in your wallet'
     } else if (step === 'approve' && !hasSufficientAllowance) {
-      buttonText = isLoading ? 'Approving...' : `Approve ${fromToken?.symbol} for spending`
+      buttonText = isLoading ? (
+        <span className='flex items-center justify-center'>
+          <svg className='animate-spin h-5 w-5 mr-3' viewBox='0 0 24 24'>
+            <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
+            <path
+              className='opacity-75'
+              fill='currentColor'
+              d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
+          </svg>
+          Approving...
+        </span>
+      ) : (
+        `Approve ${fromToken?.symbol} for spending`
+      )
       action = 'approve'
     } else if (step === 'sign') {
-      buttonText = 'Sign to swap'
+      buttonText = isLoading ? (
+        <span className='flex items-center justify-center'>
+          <svg className='animate-spin h-5 w-5 mr-3' viewBox='0 0 24 24'>
+            <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
+            <path
+              className='opacity-75'
+              fill='currentColor'
+              d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
+          </svg>
+          Sign To Swap
+        </span>
+      ) : (
+        `Sign To Swap`
+      )
       action = 'sign'
     } else if (step === 'swap') {
       buttonText = 'Confirm Swap'
