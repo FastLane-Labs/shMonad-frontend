92277264f23a7493cac33b8d467b509030c0f6be
Eddie Salcedo <inscentia@yahoo.com>
2024-08-15 15:15:45 -0700

    update iux text

--- src/components/Modals/IUX.tsx
+++ src/components/Modals/IUX.tsx
@@ -11,32 +11,36 @@ const IUXModal: React.FC<IUXModalProps> = ({ isVisible, onClose }) => {
   }
   return (
     <ModalWrapper isVisible={isVisible} onClose={closeModal} style={{ paddingBottom: '28px' }}>
-      <div className='flex overflow-scroll md:overflow-visible -mr-2.5 md:mr-0 text-neutral-content flex-col grid-cols-1 justify-start pt-4 gap-4 xl:pl-0 xl:pr-0'>
-        <h2 className='text-5xl'>MEV Powered Swap Quotes</h2>
-        <div className='gray-text text-xl'>
-          Boost your swap quotes lorem ipsum dolor sit amet, consectetur adipiscing elit
-        </div>
-        <h4 className='text-2xl font-semibold'>Ut porta eu RocketBoost</h4>
-        <div className='gray-text'>
-          Sit amet posuere lectus egestas non. Mauris tempor justo vitae risus venenatis, at aliquam diam Atlas by
-          FastLane Labs.
-          <a className='text-primary ml-1' href='/'>
-            Learn more
-            <svg
-              className='cursor-pointer inline-block w-3 h-3 ml-1 lg:h-4  lg:w-4'
-              fill='none'
-              stroke='currentColor'
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/svg'>
-              <path
-                d='m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25'
+      <div className='flex overflow-scroll md:overflow-visible -mr-2.5 md:mr-0 text-neutral-content flex-col justify-start pt-4 gap-3 xl:pl-0 xl:pr-0'>
+        <h2 className='text-5xl'>The Bots Work for Us Now</h2>
+        <h4 className='text-xl font-medium'>Fully onchain best execution for your Polygon swaps using Atlas.</h4>
+        <div className='flex flex-col gap-1.5 gray-text'>
+          <p>
+            Rocketboost Swaps are powered by the DRFQ Atlas module built by us at FastLane Labs, which hosts a fully
+            onchain request-for-quote auction for your swaps. Atlas is a generalized execution abstraction protocol for
+            the EVM used for building intent and MEV auctions.
+          </p>
+          <p>
+            Your swaps are gossipped permissionlessly to searchers over the Polygon mempool, who actually frontrun you
+            (yes frontrun you) but in doing so compete to give you the best price in the form of Rocketboost rebates.
+            {/* <a className='text-primary ml-1' href='/'>
+              Learn more
+              <svg
+                className='cursor-pointer inline-block w-3 h-3 ml-1 lg:h-4  lg:w-4'
                 fill='none'
                 stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              />
-            </svg>
-          </a>
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  d='m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25'
+                  fill='none'
+                  stroke='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                />
+              </svg>
+            </a> */}
+          </p>
         </div>
         <div className='relative w-full rounded-2xl md:overflow-hidden'>
           <Image
