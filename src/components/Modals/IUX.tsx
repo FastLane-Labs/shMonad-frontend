6a2f411305ad8eefb6b08d418cce9c96518a03f1
Eddie Salcedo <inscentia@yahoo.com>
2024-08-14 17:00:12 -0700

    ui: update rocketboost branding logo and animation to rb beta

--- src/components/Modals/IUX.tsx
+++ src/components/Modals/IUX.tsx
@@ -40,7 +40,7 @@ const IUXModal: React.FC<IUXModalProps> = ({ isVisible, onClose }) => {
         </div>
         <div className='relative w-full rounded-2xl md:overflow-hidden'>
           <Image
-            src='/rboost-gif.gif'
+            src='/rboost-beta-gif.gif'
             alt='RocketBoost branding graphic'
             layout='responsive'
             width={500}
