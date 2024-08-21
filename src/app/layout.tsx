b84a32775662b59e0b6440c54fc8acdbabd05ad5
Eddie Salcedo <inscentia@yahoo.com>
2024-08-21 15:29:54 -0700

    add title tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout(props: PropsWithChildren) {
   return (
     <html lang='en' className='bg-[#0f0f0f]'>
       <head>
+        <title>Rocketboost</title>
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta
