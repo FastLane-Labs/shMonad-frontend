54864c44b59dec260b2260482ea840c17d05e3ee
0xNotMe <nme15500@gmail.com>
2024-08-23 15:24:22 +1000

    chore: update dapp and atlas address

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -4,13 +4,13 @@ export const CHAIN_CONFIG: { [chainId: number]: ChainConfig } = {
   137: {
     contracts: {
       atlas: {
-        address: '0xCD2724Fd9079Bd57278F02808cB11848c318577d',
+        address: '0x912AceADa1b9c9B378894D0610C5684167710FDD',
       },
       atlasVerification: {
-        address: '0xf61F7B507b21513D51DC2a0dbFc950F4801605a5',
+        address: '0x2fBF38a38D753E4ce398000CCC552Efa50702e1e',
       },
       dappAddress: {
-        address: '0x87f9B954C403Fec2A9964CFFB8AcA9CD7ED55FDf',
+        address: '0x498aC70345AD6b161eEf4AFBEA8F010401cfa780',
       },
       multicall3: {
         address: '0xcA11bde05977b3631167028862bE2a173976CA11',
@@ -20,7 +20,7 @@ export const CHAIN_CONFIG: { [chainId: number]: ChainConfig } = {
       name: 'AtlasVerification',
       version: '1.0',
       chainId: 137,
-      verifyingContract: '0xf61F7B507b21513D51DC2a0dbFc950F4801605a5',
+      verifyingContract: '0x2fBF38a38D753E4ce398000CCC552Efa50702e1e',
     },
     blockExplorerUrl: 'https://polygonscan.com/',
   },
