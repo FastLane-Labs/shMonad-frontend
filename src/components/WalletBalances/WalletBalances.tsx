d06e219c58366d89d92271e15f3dda4c52c828a1
Eddie Salcedo <inscentia@yahoo.com>
2025-01-05 00:52:41 -0800

    add wallet balance component

--- src/components/WalletBalances/WalletBalances.tsx
+++ src/components/WalletBalances/WalletBalances.tsx
@@ -1,15 +1,18 @@
-import React, { useEffect, useMemo } from 'react'
+import { useState, useEffect, useMemo } from 'react'
+import { ethers } from 'ethers'
+import SettingsButton from '@/components/Buttons/SettingsButton'
+import SettingsModal from '@/components/Modals/SettingsModal'
 import { TokenBalance } from '@/components/TokenBalance/TokenBalance'
-import SellAmount from './SellAmount'
 import { useSwapStateContext } from '@/context/SwapStateContext'
 import { useAccount } from 'wagmi'
+import { useCurrentTokenList } from '@/hooks/useTokenList'
 import { useBalance } from '@/hooks/useBalance'
 import { Token } from '@/types'
-import { ethers } from 'ethers'
-import { useCurrentTokenList } from '@/hooks/useTokenList'
 import { useTokenUsdPrice } from '@/hooks/useTokenUsdPrice'
 
-const SellComponent: React.FC = () => {
+const WalletBalances: React.FC = () => {
+  const [isSettingsModalVisible, setIsSettingsModalVisible] = useState<boolean>(false)
+
   const {
     fromToken: sellToken,
     setFromToken: setSellToken,
@@ -63,32 +66,40 @@ const SellComponent: React.FC = () => {
       setSwapDirection('sell')
     }
   }
-
   return (
-    <div className='input-card mb-4'>
-      <div className='flex justify-between items-center mb-2 text-sm'>
-        <span className='text-base-content'>Enter Monad amount</span>
-        <div className='flex text-base-content gap-0.5 items-center justify-center'>
-          {/* <span>Balance: </span>
-          <TokenBalance token={sellToken || undefined} toFixed={3} /> */}
-          <button className='max-button text-[15px]' onClick={handleSetMax}>
-            MAX
-          </button>
+    <div
+      className='relative rounded-t-3xl bg-base-200/30 -mb-5'
+      style={{ boxShadow: 'rgba(241, 32, 116, .2) 0px 5px 90px 4px' }}>
+      <div className='gradient-bg relative max-w-md mx-auto p-4 rounded-t-3xl pb-9 border-t border-x border-accent'>
+        <div className='flex justify-between items-center mb-2 space-x-2'>
+          <div className='flex w-full space-x-2'>
+            <button className='btn btn-menu !w-2/5'>Mint</button>
+            <button className='btn btn-menu !w-2/5'>Bond / Unbond</button>
+          </div>
+          <SettingsButton setIsSettingsModalVisible={setIsSettingsModalVisible} />
         </div>
-      </div>
-      <SellAmount
-        sellToken={sellToken}
-        setSellToken={setSellToken}
-        sellAmount={sellAmount}
-        setSellAmount={setSellAmount}
-        quoteLoading={isQuoteing}
-        setSwapDirection={setSwapDirection}
-      />
-      <div className='text-left mt-2 text-sm text-base-content h-5'>
-        {usdValue !== null ? `$${usdValue.toFixed(2)}` : '\u00A0'}
+        <SettingsModal isVisible={isSettingsModalVisible} onClose={() => setIsSettingsModalVisible(false)} />
+        <div className=''>
+          <div className='pt-2'>
+            <span className='text-xs'>Available to Mint: </span>
+            <div className='text-lg font-extrabold'>
+              <TokenBalance token={sellToken || undefined} toFixed={3} />
+              <span> MONAD</span>
+            </div>
+          </div>
+          <div className='line-seperator-sm mt-2' />
+
+          <div className='pt-2'>
+            <span className='text-xs'>SHMONAD Balance: </span>
+            <div className='text-lg font-extrabold'>
+              <TokenBalance token={sellToken || undefined} toFixed={3} />
+              <span> SHMONAD</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
-export default SellComponent
+export default WalletBalances
