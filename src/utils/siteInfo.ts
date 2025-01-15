4587a236af722d0b58c9409535b88653333d9b99
Eddie Salcedo <inscentia@yahoo.com>
2025-01-14 18:48:09 -0800

    update siteInfo

--- src/utils/siteInfo.ts
+++ src/utils/siteInfo.ts
@@ -1,6 +1,6 @@
-export const SITE_EMOJI = '🚀'
-export const SITE_NAME = 'RocketBoost'
+export const SITE_EMOJI = '💜'
+export const SITE_NAME = 'shMonad'
 export const SITE_INFO = ''
 export const SITE_DESCRIPTION =
-  'Rocketboost Swaps are powered by the DRFQ Atlas module built by FastLane Labs, which hosts a fully onchain request-for-quote auction for your swaps.'
-export const SITE_URL = 'http://rocketboost.me'
+  'shmonad.xyz is a decentralized application enabling users to mint, bond, unbond, and manage shMONAD tokens by interacting with smart contracts directly on the blockchain.'
+export const SITE_URL = 'http://shmonad.xyz'
